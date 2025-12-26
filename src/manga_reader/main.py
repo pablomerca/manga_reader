4e40353fa082678f68cdb1c472a24203a3f64b9a
Pablo Mercado <pablomercado0123@gmail.com>
2025-12-26 01:28:28 -0300

    Creates Initial Folder Structure.

---
+++ src/manga_reader/main.py
@@ -0,0 +1,10 @@
+"""Main entry point for the manga reader application."""
+
+def main():
+    """Launch the manga reader application."""
+    print("Manga Reader application starting...")
+    # TODO: Initialize your application here
+
+
+if __name__ == "__main__":
+    main()
